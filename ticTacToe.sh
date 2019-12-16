38475687a18325caf7505501507c838e256fe65d
santoshingale <santoshingale876@gmail.com>
2019-12-16 12:44:13 +0530

    Added : Function to print and validate the cell

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 echo "Welcome to TicTacToe game"
-
+declare -a board
 board=(1 2 3 4 5 6 7 8 9)
 
 function getLetterAssigment(){
@@ -17,11 +17,44 @@ function toss(){
 
 	if [ $player == 'X' ]
 	then
-		echo player
+		echo X
 	else
-		echo computer
+		echo O
 	fi
 }
+function printBoard(){
+	echo "-------------"
+	for((i=0;i<9;i+=3))
+	do
+		echo "| "${board[$i]}" | "${board[ $i + 1 ]}" | "${board[ $i + 2 ]}" |"
+		echo "-------------"
+	done
+}
+function setCell(){
+	printBoard
+
+	if [ $player == $turn ]
+	then
+
+		read -p "Enter Cell number" cell
+
+		if [[ $cell -gt 9 || $cell -lt 0 ]]
+		then
+			echo "Input is invalid"
+			setCell
+		elif [ ${board[$cell-1]} -eq $cell ]
+		then
+			board[$cell-1]=X
+		else
+		echo "Input is invalid or Cell is pre allocated"
+		setCell
+		fi
+
+	fi
+}
+#setCell
+#printBoard
 turn=$(toss)
 echo $turn
-
+setCell
+printBoard
