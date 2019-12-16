415c65ea6f70b4f7962e4b8677f64ddbddca12b2
santoshingale <santoshingale876@gmail.com>
2019-12-16 10:36:08 +0530

    Added: Function which generate random number and return X or O letter

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,3 +2,13 @@
 echo "Welcome to TicTacToe game"
 
 board=(1 2 3 4 5 6 7 8 9)
+
+function getLetterAssigment(){
+
+	case $(( RANDOM % 2 )) in
+		1) echo X;;
+		0) echo O;;
+	esac
+}
+
+player=$( getLetterAssigment )
